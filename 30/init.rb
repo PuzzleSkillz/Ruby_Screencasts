3ada929a10dd186bca1692f64edf30faea323da9
Puzzle <puzzleskillz@gmail.com>
2016-09-29 04:02:12 +0300

    30

---
+++ 30/init.rb
@@ -0,0 +1,12 @@
+require_relative "string"
+require_relative "item_container"
+require_relative "cart"
+require_relative "order"
+require_relative "item"
+require_relative "virtual_item"
+require_relative "real_item"
+
+@items = []
+@items << RealItem.new({:price => 101, :weight =>100, :name => "car" })
+@items << RealItem.new({:weight =>100, :price => 101, :name => "kettle" })
+@items << RealItem.new({:weight =>100, :price => 101, :name => "dishwasher" })
